f23535346961650c7c1873e96375ec5aec8eb81c
phu <vuphu30908@gmail.com>
2025-06-06 15:04:33 +0700

    membership

---
+++ src/Model/membershipDAO.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+import Service.dbConnection;
+import java.sql.*;
+/**
+ *
+ * @author ADMIN
+ */
+public class membershipDAO {
+    public ResultSet hienthi(){
+        try {
+            Connection conn=dbConnection.connect();
+            String query = "select * from membership";
+            PreparedStatement ps = conn.prepareStatement(query);
+            return ps.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+      public boolean add(String membership_name, String phone, String security_code, String rank_name, Timestamp expiration_date) {
+        try {
+            Connection conn = dbConnection.connect(); 
+            String query = "INSERT INTO membership (membership_name, phone, sercurity_code, rank_name, expiration_date) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setString(1, membership_name);
+            ps.setString(2, phone);
+            ps.setString(3, security_code);
+            ps.setString(4, rank_name);
+            ps.setTimestamp(5, expiration_date);
+
+            int rows = ps.executeUpdate();
+            return rows > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+}
